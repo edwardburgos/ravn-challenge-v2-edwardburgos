54f0d9d6a2bd7746d5f10a016cac9710cd1943c4
EdwardBurgos <edwardpbn@gmail.com>
2021-11-12 12:19:49 -0500

    apollo implemented

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+// Apollo client definition
+const client = new ApolloClient({
+  // Here we can define many properties
+  uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index', // API url
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
